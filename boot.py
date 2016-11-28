f8ecbc26d88d85f2431503e7585b76176024922c
avalon91 <lucaspwo@gmail.com>
2016-11-28 10:56:22 -0300

    Primeiro commit
    Arquivos para controle de acesso ao LAR, conexão às redes Wi-Fi e
    controle de LEDs WS2812B

---
+++ boot.py
@@ -0,0 +1,30 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import gc
+import machine
+import conectar
+import time
+import os
+import webrepl
+webrepl.start()
+gc.collect()
+
+# if conectar.conectar():#
+#     onboardled = machine.PWM(machine.Pin(2), freq=500, duty=1000)#
+#     time.sleep(1)#
+#     for i in range(1001, 1024):#
+#         onboardled.duty(i)#
+#         time.sleep_ms(50)#
+# else:#
+#     onboardled = machine.PWM(machine.Pin(2), freq=500, duty=0)#
+#     time.sleep(1)#
+#     onboardled.duty(1023)#
+#     time.sleep(1)#
+#     onboardled.duty(0)#
+#     time.sleep(1)#
+#     onboardled.duty(1023)#
+#     time.sleep(1)#
+#     onboardled.duty(0)# 
+#     time.sleep(1)#
+#     onboardled.duty(1023)
---
+++ mqtt_rfid.py
@@ -0,0 +1,82 @@
+#https://github.com/micropython/micropython-lib/tree/master/umqtt.simple
+#https://github.com/micropython/micropython-lib/blob/master/umqtt.simple/example_pub.py
+#https://github.com/micropython/micropython-lib/pull/91#issuecomment-239030008
+
+def conectar():
+    #import dht
+    import mfrc522
+    from os import uname
+    import machine
+    import time
+    #d = dht.DHT11(machine.Pin(13))
+    from umqtt.simple import MQTTClient
+    #import micropython
+
+    if uname()[0] == 'WiPy':
+        rdr = mfrc522.MFRC522("GP14", "GP16", "GP15", "GP22", "GP17")
+    elif uname()[0] == 'esp8266':
+        rdr = mfrc522.MFRC522(0, 2, 4, 5, 14)
+    else:
+        raise RuntimeError("Unsupported platform")
+
+    SERVER = "10.6.1.112"
+    TOPIC = b"/rfid/normal"
+    #TOPIC1 = b"/esp/dht/temp"
+    #TOPIC2 = b"/esp/dht/hum"
+    ID = "esp"
+    USER = b"esp"
+    PASSWORD = b"senhaesp"
+
+    c = MQTTClient(ID, SERVER, user=USER, password=PASSWORD)
+    #c.connect()
+
+    while True:
+        uid = ""
+    	# print("")
+    	# print("Place card before reader to read from address 0x08")
+    	# print("")
+
+    	(stat, tag_type) = rdr.request(rdr.REQIDL)
+
+    	if stat == rdr.OK:
+
+    		(stat, raw_uid) = rdr.anticoll()
+
+    		if stat == rdr.OK:
+    			#print("New card detected")
+    			#print("  - tag type: 0x%02x" % tag_type)
+    			#print("%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
+    			for i in range(0, 4):
+    				uid = uid + "%02x" % raw_uid[i]
+                uid = uid + "0"
+                print(uid)
+                c.connect()
+                c.publish(TOPIC, b"%s" % uid)
+                c.disconnect()
+
+    			# if rdr.select_tag(raw_uid) == rdr.OK:
+    			#
+    			# 	key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+    			#
+    			# 	if rdr.auth(rdr.AUTHENT1A, 8, key, raw_uid) == rdr.OK:
+    			# 		print("Address 8 data: %s" % rdr.read(8))
+    			# 		rdr.stop_crypto1()
+    			# 	else:
+    			# 		print("Authentication error")
+    			# else:
+    			# 	print("Failed to select tag")
+
+
+        #try:
+        #c.connect()
+        #d.measure()
+        #temp = d.temperature()
+        #hum = d.humidity()
+        #print('Temp: %s' % temp)
+        #print('Hum: %s' % hum)
+        #c.publish(TOPIC, str(uid))
+        #c.publish(TOPIC2, str(hum))
+        #c.disconnect()
+        #time.sleep(30) #30 segundos
+        #finally:
+            #c.disconnect()
