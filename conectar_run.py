9ae07dd334b28735ab6e7ba5b916d24f0d9ee0d9
Lucas Oliveira <lucaspwo@gmail.com>
2016-12-09 00:06:36 -0300

    Códigos para neopixel

---
+++ conectar_run.py
@@ -0,0 +1,55 @@
+import network
+import time
+flag = False
+count = 0
+wlan = network.WLAN(network.STA_IF)
+if not wlan.isconnected():
+    wlan.active(True)
+    wlan.connect('Rockbox Turbo', 'sacidepatinete')
+    print('Conectando na rede Rockbox Turbo...')
+    #wlan.connect('BonoNet', 'bonooliveira402')
+    #print('Conectando na rede BonoNet...')
+    for i in range(0, 9):
+        if wlan.isconnected():
+            flag = True
+            count = 0
+            break
+        count = count + 1
+        time.sleep(1)
+    if count == 9:
+        count = 0
+        wlan.connect('BonoNet', 'bonooliveira402')
+        print('Conectando na rede BonoNet...')
+        #wlan.connect('LAR-ECT', 'senhafacil1234')
+        #print('Conectando na rede LAR-ECT...')
+        for i in range(0, 9):
+            if wlan.isconnected():
+                flag = True
+                count = 0
+                break
+            count = count + 1
+            time.sleep(1)
+    if count == 9:
+        count = 0
+        wlan.connect('LAR-ECT', 'senhafacil1234')
+        print('Conectando na rede LAR-ECT...')
+        #wlan.connect('Rockbox Turbo', 'sacidepatinete')
+        #print('Conectando na rede Rockbox Turbo...')
+        for i in range(0, 9):
+            if wlan.isconnected():
+                flag = True
+                break
+            time.sleep(1)
+if flag == True:
+    print('Conectado')
+    print(wlan.ifconfig())
+    #return True
+elif wlan.isconnected():
+    print('Conectado')
+    print(wlan.ifconfig())
+    #return True
+elif flag == False:
+    print('Conexao falhou')
+    #return False
+    #while not wlan.isconnected():
+        #pass
