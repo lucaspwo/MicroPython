2acc4f946369e53b3d93c6e67d9e9a8fa707c2b2
Lucas Oliveira <avalon91@users.noreply.github.com>
2018-10-04 13:54:39 -0300

    Atual. script

--- Sensores_Anet/run.py
+++ Sensores_Anet/run.py
@@ -1,48 +1,63 @@
-import time, utime, machine, onewire, ds18x20, network
-from umqtt.simple import MQTTClient
+def run():
+    import time, utime, machine, onewire, ds18x20, network, neopixel
+    from umqtt.simple import MQTTClient
 
-wlan = network.WLAN(network.STA_IF)
-while not wlan.isconnected():
-    utime.sleep(1)
+    wlan = network.WLAN(network.STA_IF)
+    while not wlan.isconnected():
+        utime.sleep(1)
 
-SERVER = "206.189.191.14"
-TOPIC1 = b"/esp/temp"
-TOPIC2 = b"/esp/smoke"
-TOPICR = b"/esp/daymsgr"
-TOPICS = b"/esp/daymsgs"
-ID = "esp"
+    rele = machine.Pin(14, machine.Pin.OUT)
+    rele.off()
 
-mens = ''
+    np = neopixel.NeoPixel(machine.Pin(5), 10)
+    n = np.n
 
-def sub_cb(topic, msg):
-    global mens
-    mens = msg.decode("utf-8")
+    for i in range(n):
+        np[i] = (127,127,127)
 
-dat = machine.Pin(5)
-ds = ds18x20.DS18X20(onewire.OneWire(dat))
-roms = ds.scan()
+    np.write()
 
-adc = machine.ADC(0)
+    relay = ''
 
-c = MQTTClient(ID, SERVER)
-c.set_callback(sub_cb)
-c.connect()
-c.subscribe(TOPICR)
+    def sub_cb(topic, msg):
+        global relay
+        relay = msg.decode("utf-8")
+        if(relay == '1'):
+            rele.on()
+        if(relay == '0'):
+            rele.off()
 
+    SERVER = "206.189.191.14"
+    TOPIC1 = b"/esp/temp"
+    TOPIC2 = b"/esp/smoke"
+    TOPICS = b"/esp/relay/mcu"
+    TOPICR = b"/esp/relay/node"
+    ID = "esp"
 
-while True:
+    dat = machine.Pin(4)
+    ds = ds18x20.DS18X20(onewire.OneWire(dat))
+    roms = ds.scan()
 
-    c.check_msg()
-    ds.convert_temp()
-    tmp = ds.read_temp(roms[0])
-    stmp = str(tmp)
-    btmp = stmp.encode()
-    smk = adc.read()
-    ssmk = str(smk)
-    bsmk = ssmk.encode()
-    print('temp: ', tmp)
-    print('mq2: ', smk)
-    c.publish(TOPIC1, btmp)
-    c.publish(TOPIC2, bsmk)
-    c.publish(TOPICS, mens.encode())
-    time.sleep_ms(2000)
+    adc = machine.ADC(0)
+
+    c = MQTTClient(ID, SERVER)
+    c.set_callback(sub_cb)
+    c.connect()
+    c.subscribe(TOPICR)
+
+    while True:
+
+        c.check_msg()
+        ds.convert_temp()
+        time.sleep_ms(750)
+        tmp = ds.read_temp(roms[0])
+        stmp = str(tmp)
+        btmp = stmp.encode()
+        smk = adc.read()
+        ssmk = str(smk)
+        bsmk = ssmk.encode()
+        print('temp: ', tmp)
+        print('mq2: ', smk)
+        c.publish(TOPIC1, btmp)
+        c.publish(TOPIC2, bsmk)
+        #time.sleep_ms(2000)
